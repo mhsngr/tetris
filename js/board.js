b8fe5014176ca5cf1ba2be52743ff50c286b4799
mhsngr <markus.heisinger@gmail.com>
2021-04-05 13:19:40 +0200

    add draw function to board

--- js/board.js
+++ js/board.js
@@ -6,8 +6,26 @@ class Board {
             this.grid[i].fill(0);
         }
     }
+    // create() {
+    //     let board = document.getElementsByClassName('board');
+    //     for(let i = 0; i < this.grid.length; i++) {
+    //         let col = document.createElement('div');
+    //         col.className = `col ${i}`;
+    //         for(let j = 0; j < this.grid[i].length; j++) {
+    //             let row = document.createElement('div');
+    //             row.className = `row ${j}`;
+    //             col.appendChild(row)
+    //         }
+    //         board.appendChild(col);
+    //     }
+    // }
     draw() {
         console.table(this.grid);
+        for(let i = 0; i < this.grid.length; i++) {
+            for(let j = 0; j < this.grid[i].length; j++) {
+                document.querySelector(`.board > .row.y${i} > .col.x${j}`).innerText = `${this.grid[i][j]}`;
+            }
+        }
     }
 }
 
